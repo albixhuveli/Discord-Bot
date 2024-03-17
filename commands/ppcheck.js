6ae2e5cb08fbf9b94fc04b65f09522fbfb43b45d
albixhuveli <albi.xhuveli.601@gmail.com>
2024-03-16 20:34:23 -0400

    updating npm and starting base for new features

--- commands/ppcheck.js
+++ commands/ppcheck.js
@@ -1,13 +1,13 @@
 module.exports= {
-    name: 'checkpp',
+    name: 'checkpriv',
     description : "pp checker",
     execute(message, args) {
         
         if(message.member.roles.cache.has('847325724896526397')){
-            message.channel.send('fat cock confirmed!');
+            message.channel.send('priv confirmed!');
             
         } else {
-            message.channel.send('no pp detected.');
+            message.channel.send('priv detected.');
         }
             
 
