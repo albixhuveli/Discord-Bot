f429df3666d9f3a49eac6c8432b358189c9e059a
albixhuveli <albi.xhuveli.601@gmail.com>
2024-03-16 20:35:21 -0400

    updating npm and starting base for new features

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({ intents: [gateway] })
 const prefix = '!';
 const fs = require ('fs');
 client.commands = new Discord.Collection();
