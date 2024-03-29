48f01454c95709338d09b978acfc5cdb6e857094
albixhuveli <albi.xhuveli.601@gmail.com>
2024-03-29 15:46:35 -0400

    cleanup api code

--- commands/utility/rlstats.js
+++ commands/utility/rlstats.js
@@ -29,25 +29,11 @@ module.exports = {
 	async execute(interaction) {
     const apiUrl = 'https://api.tracker.gg/api/v2/rocket-league/standard/profile/epic/grayehz?'
     fetch(apiUrl, {
-    "headers": {
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "accept-language": "en-US,en;q=0.9",
-    "cache-control": "max-age=0",
-    "if-modified-since": "Wed, 27 Mar 2024 00:22:33 GMT",
-    "sec-ch-ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": '"Windows"',
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "none",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1"
-      },
-    "referrerPolicy": "strict-origin-when-cross-origin",
-    "body": null,
-    "method": "GET",
-    "mode": "cors",
-    "credentials": "include"
+      "headers": {
+        
+      "sec-ch-ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+
+      }
     })
 
     .then(response => {
