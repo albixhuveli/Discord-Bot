39706b106811df0c32ae0e44985f27bbcd370ba2
albixhuveli <albi.xhuveli.601@gmail.com>
2024-04-01 23:40:04 -0400

    added chat options integration also deployed reload to deploy-commands file

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -5,10 +5,19 @@ const wait = require('node:timers/promises').setTimeout;
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Replies with Pong!')
+		.addStringOption(option =>
+			option
+				.setName('target')
+				.setDescription('target word')
+				.setRequired(true)
+				),
+
 	async execute(interaction) {
-		await interaction.reply({ content: 'pong!', ephemeral: true });
-		await wait(50_000);
+		const target = interaction.options.getString('target') ?? 'No target provided';
+		await interaction.reply({ content: 'pong! ' + target, ephemeral: true });
+		await wait(30_000);
 		await interaction.deleteReply();
 	},
 };
+
--- commands/utility/rlstats.js
+++ commands/utility/rlstats.js
@@ -1,16 +1,16 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 // extraction from metadata
-function extractPercetile(userData, gameMode = "Ranked Doubles 2v2") {
+//target == gameMode = "Ranked Doubles 2v2"
+function extractPercetile(userData, target = "Ranked Doubles 2v2") {
   const segments = userData['data']['segments']
-
   let currentSeason = userData['data']['metadata']['currentSeason']; // current season i.e. 28
   let percentile = -1;
   
   for (var segment of segments) {
     if (segment['type'] == 'playlist'){
       if(segment['attributes']['season'] == currentSeason)  {
-        if(segment['metadata']['name'] == gameMode){
+        if(segment['metadata']['name'] == target){
           percentile = segment['stats']['tier']['percentile'];
           return percentile;
         }
@@ -18,21 +18,37 @@ function extractPercetile(userData, gameMode = "Ranked Doubles 2v2") {
     }
   }
   console.log('percentile:', percentile);
-  return gameMode;
+  console.log('target:', target);
+  return target;
 }
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('rl')
-		.setDescription('rlstats'),
+		.setDescription('rlstats')
+    .addStringOption(option => 
+      option.setName('target')
+        .setDescription('target gamemode to search')
+        .setRequired(true)
+        .addChoices(
+					{ name: '2v2', value: 'Ranked Doubles 2v2' },
+					{ name: '3v3', value: 'Ranked Standard 3v3' },
+					{ name: '1v1', value: 'Ranked Duel 1v1' },
+          )
+    // .addStringOption(option => 
+    //   option.setName('user')
+    //     .setDescription('user to search')
+    //     .setRequired(true)
+    //   )
+    ), 
+        
   // api call  
 	async execute(interaction) {
+
     const apiUrl = 'https://api.tracker.gg/api/v2/rocket-league/standard/profile/epic/grayehz?'
     fetch(apiUrl, {
       "headers": {
-        
       "sec-ch-ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
-
       }
     })
 
@@ -49,7 +65,7 @@ module.exports = {
       let percentile = extractPercetile(userData);
       console.log(percentile);
       console.log(typeof percentile);
-      await interaction.reply({ content: percentile.toString() });
+      await interaction.reply({ content: percentile.toString(), });
     })
     .catch(error => {
       console.error('Error:', error);
