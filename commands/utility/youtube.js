244a0620ff6d136291b04c64bc2f59bdfac85725
albixhuveli <albi.xhuveli.601@gmail.com>
2024-04-16 02:15:46 -0400

    added option to search URL or fuzzy

--- commands/utility/youtube.js
+++ commands/utility/youtube.js
@@ -3,8 +3,6 @@ const { ytapi } = require('../../config.json');
 const FuzzySearch = require('fuzzy-search');
 const axios = require('axios');
 
-// extraction from metadata
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('youtube')
@@ -13,22 +11,28 @@ module.exports = {
         .addStringOption(option => 
             option
                 .setName('search')
-                .setDescription('video to search')
+                .setDescription('video to search or url')
                 .setRequired(true)
-            ), 
+        ), 
 
   // api call  
 	async execute(interaction) {
     const search = interaction.options.getString('search') ?? 'No user provided';
-    const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${search}&key=` + ytapi);
-    const videos = response.data.items.map(item => ({ title: item.snippet.title, url: `https://www.youtube.com/watch?v=${item.id.videoId}` }));
-    const searcher = new FuzzySearch(videos, ['title'], { caseSensitive: false });
-    const result = searcher.search(search);
+    const urlPattern = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/;
+    if (urlPattern.test(search)) {
+			// Handle the URL here. For example, you could validate the URL and then send it back to the user.
+			await interaction.reply(`You provided this URL: ${search}`);
+		} else {
+      const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${search}&key=` + ytapi);
+      const videos = response.data.items.map(item => ({ title: item.snippet.title, url: `https://www.youtube.com/watch?v=${item.id.videoId}` }));
+      const searcher = new FuzzySearch(videos, ['title'], { caseSensitive: false });
+      const result = searcher.search(search);
 
-    if (result.length > 0) {
-      await interaction.reply(`Top result: ${result[0].title} - ${result[0].url}`);
-    } else {
-      await interaction.reply('No results found');
+      if (result.length > 0) {
+        await interaction.reply(`Top result: ${result[0].title} - ${result[0].url}`);
+      } else {
+        await interaction.reply('No results found');
+      }
     }
   },
 }
